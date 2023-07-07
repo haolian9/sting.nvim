87f68f1752be9f42611c6da367d85e1f8f3cf631
haoliang <haoliang0.1.2@gmail.com>
2023-07-07 22:41:41 +0800

    sync with upstream

--- lua/sting/rhs.lua
+++ lua/sting/rhs.lua
@@ -13,46 +13,47 @@ do
     right = "belowright vsplit",
   }
 
-  local function split(mode, bufnr)
-    ex("wincmd", "p")
-    ex(assert(splitcmds[mode]))
-    local winid = api.nvim_get_current_win()
-    api.nvim_win_set_buf(winid, bufnr)
-  end
-
-  local function find_quickfix_current_bufnr()
-    local curline = vim.fn.getqflist({ idx = 0 }).idx
-    if curline < 1 then return jelly.debug("no lines in current quickfix list") end
-    ---@type sting.Pickle
-    local pickle = vim.fn.getqflist({ idx = curline, items = 0 }).items[1]
-    return assert(pickle.bufnr)
-  end
-
-  local function find_location_current_bufnr()
-    ---luckily, this magic winid=0 will be converted to lastwin
-    local curline = vim.fn.getloclist(0, { idx = 0 }).idx
-    if curline < 1 then return jelly.debug("no lines in current location list") end
-    ---@type sting.Pickle
-    local pickle = vim.fn.getloclist(0, { idx = curline, items = 0 }).items[1]
-    return assert(pickle.bufnr)
-  end
-
   ---@param mode 'above'|'below'|'left'|'right'
   function M.split(mode)
     local winid = api.nvim_get_current_win()
-    local wininfo = vim.fn.getwininfo(winid)[1]
+
     local bufnr
-    ---must check loclist first, as .quickfix=1 in both location and quickfix window
-    if wininfo.loclist == 1 then
-      bufnr = find_location_current_bufnr()
-    elseif wininfo.quickfix == 1 then
-      bufnr = find_quickfix_current_bufnr()
-    else
-      jelly.err("winid=%d, wininfo=%s", winid, vim.inspect(wininfo))
-      error("supposed to be in a quickfix/location window")
+    do
+      local wininfo = vim.fn.getwininfo(winid)[1]
+      local expect_idx = api.nvim_win_get_cursor(winid)[1]
+      if wininfo.loclist == 1 then ---must check loclist first, as .quickfix=1 in both location and quickfix window
+        local held_idx = vim.fn.getqflist({ idx = 0 }).idx
+        if held_idx < 1 then return jelly.debug("no lines in current quickfix list") end
+        if held_idx ~= expect_idx then
+          vim.fn.setloclist({}, "a", { idx = expect_idx })
+          held_idx = expect_idx
+        end
+        ---@type sting.Pickle
+        local pickle = vim.fn.getqflist({ idx = held_idx, items = 0 }).items[1]
+        bufnr = assert(pickle.bufnr)
+      elseif wininfo.quickfix == 1 then
+        local held_idx = vim.fn.getloclist(0, { idx = 0 }).idx
+        if held_idx < 1 then return jelly.debug("no lines in current location list") end
+        if held_idx ~= expect_idx then
+          vim.fn.setqflist({}, "a", { idx = expect_idx })
+          held_idx = expect_idx
+        end
+        ---@type sting.Pickle
+        local pickle = vim.fn.getloclist(0, { idx = held_idx, items = 0 }).items[1]
+        bufnr = assert(pickle.bufnr)
+      else
+        jelly.err("winid=%d, wininfo=%s", winid, vim.inspect(wininfo))
+        error("supposed to be in a quickfix/location window")
+      end
+      assert(bufnr ~= nil)
     end
-    if bufnr == nil then return end
-    split(mode, bufnr)
+
+    do
+      ex("wincmd", "p")
+      ex(assert(splitcmds[mode]))
+      local winid = api.nvim_get_current_win()
+      api.nvim_win_set_buf(winid, bufnr)
+    end
   end
 end
 
